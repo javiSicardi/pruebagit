19b2ebbd9e6c3ec6cec60944e6ea54a4c3d49b8b
javier sicardi <javisicardi94@gmail.com>
2021-01-06 16:09:47 -0300

    init

---
+++ web/web.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web.web.MvcApplication" Language="C#" %>
---
+++ web/web.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(web.web.Startup))]
+namespace web.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
